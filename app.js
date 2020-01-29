9d279b42d6eccda29b90197ecb5fda2fa37bdef8
Nelly Mahecha <nejumava@gmail.com>
2020-01-29 13:53:00 -0500

    Elimina bodyparser

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded());
 app.set('view engine', 'pug');
 app.set('views', 'views');
-
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('index');
